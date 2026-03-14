e1245e16936a525bb84100dc5ee8d3aa72a0015d
jai <vjaivasanth@gmail.com>
2026-03-14 18:54:02 +0800

    Add ShowCommand.java

---
+++ src/main/java/seedu/address/logic/commands/ShowCommand.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+
+/**
+ * Clears the address book.
+ */
+public class ShowCommand extends Command {
+
+    public static final String COMMAND_WORD = "show";
+    public static final String MESSAGE_SUCCESS = "Showing details of employee";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        /*
+        need to implement model.show(String name);
+        decide whether prefix is accepted or full name is required
+         */
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
