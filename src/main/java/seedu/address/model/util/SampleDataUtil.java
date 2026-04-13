5201dc200b5c573720b1ab719c4093b0af892327
Lin Muxi <e1355399@u.nus.edu>
2026-04-14 04:37:57 +0800

    Revert sample data tag changes

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -25,22 +25,22 @@ public class SampleDataUtil {
         return new Employee[] {
             new Employee(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Department("IT"), new Position("Junior software developer"),
-                    getTagSet("fulltime"), new TaskListStorage(new ArrayList<>())),
+                    getTagSet("friends"), new TaskListStorage(new ArrayList<>())),
             new Employee(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Department("Sales"), new Position("HR manager"),
-                    getTagSet("fulltime", "mentor"), new TaskListStorage(new ArrayList<>())),
+                    getTagSet("colleagues", "friends"), new TaskListStorage(new ArrayList<>())),
             new Employee(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Department("HR"), new Position("Team leader"),
-                    getTagSet("onLeave"), new TaskListStorage(new ArrayList<>())),
+                    getTagSet("neighbours"), new TaskListStorage(new ArrayList<>())),
             new Employee(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                     new Department("Marketing"), new Position("Marketing lead"),
-                    getTagSet("fulltime"), new TaskListStorage(new ArrayList<>())),
+                    getTagSet("family"), new TaskListStorage(new ArrayList<>())),
             new Employee(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                     new Department("Finance"), new Position("Frontend developer"),
-                    getTagSet("remoteEligible"), new TaskListStorage(new ArrayList<>())),
+                    getTagSet("classmates"), new TaskListStorage(new ArrayList<>())),
             new Employee(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                     new Department("Creative"), new Position("Product manager"),
-                    getTagSet("fulltime"), new TaskListStorage(new ArrayList<>()))
+                    getTagSet("colleagues"), new TaskListStorage(new ArrayList<>()))
         };
     }
 
